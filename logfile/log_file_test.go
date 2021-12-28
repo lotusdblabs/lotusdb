95bcac8c3efca7c462e4f45315cb861b6e46786e
yzy-github <2001212833@stu.pku.edu.cn>
2021-12-28 11:23:28 +0800

    add:log_file_test.go log_mmap_test.go

--- logfile/log_file_test.go
+++ logfile/log_file_test.go
@@ -1,10 +1,3 @@
-/**
- * @Author: yzy
- * @Description:
- * @Version: 1.0.0
- * @Date: 2021/12/27 15:50
- * @Copyright: MIN-Group；国家重大科技基础设施——未来网络北大实验室；深圳市信息论与未来网络重点实验室
- */
 package logfile
 
 import (
