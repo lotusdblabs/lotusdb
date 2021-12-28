de54f75ecb0fb9d36c085a46bc2509ec3e03815d
malikhou <malikhou@didiglobal.com>
2021-12-28 15:07:21 +0800

    add read index

---
+++ index/boltdb/error.go
@@ -0,0 +1,11 @@
+package boltdb
+
+import "errors"
+
+var (
+	ErrDBNameNil     = errors.New("DB name is nil")
+	ErrBucketNameNil = errors.New("bucket name is nil")
+	ErrFilePathNil   = errors.New("file path is nil")
+	ErrTXNil         = errors.New("transaction is nil")
+	ErrTypeFalse     = errors.New("transaction type error")
+)
