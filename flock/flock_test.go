7789d2832bb9a7ccf4673252702e3d22f023bd8a
roseduan <roseduan520@gmail.com>
2022-01-08 15:41:19 +0800

    add flock test

--- flock/flock_test.go
+++ flock/flock_test.go
@@ -1,24 +1,73 @@
 package flock
 
 import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"sync"
+	"sync/atomic"
 	"testing"
-	"time"
 )
 
 func TestAcquireFileLock(t *testing.T) {
-	acquire := func() {
+	testFn := func(readOnly bool, times int, actual int) {
 		path := "/tmp/FLOCK"
-		_, err := AcquireFileLock(path, false)
-		if err != nil {
-			t.Log("acquire flock err.", err)
-		} else {
-			t.Log("acquire flock success")
+		defer os.Remove(path)
+		var count uint32
+
+		wg := &sync.WaitGroup{}
+		wg.Add(times)
+		for i := 0; i < times; i++ {
+			go func() {
+				defer wg.Done()
+				_, err := AcquireFileLock(path, readOnly)
+				if err != nil {
+					atomic.AddUint32(&count, 1)
+				}
+			}()
 		}
+		wg.Wait()
+		assert.Equal(t, count, uint32(actual))
 	}
 
-	go acquire()
-	go acquire()
-	go acquire()
+	t.Run("exclusive-1", func(t *testing.T) {
+		testFn(false, 1, 0)
+	})
 
-	time.Sleep(time.Minute)
+	t.Run("exclusive-2", func(t *testing.T) {
+		testFn(false, 10, 9)
+	})
+
+	t.Run("exclusive-3", func(t *testing.T) {
+		testFn(false, 500, 499)
+	})
+
+	t.Run("shared-1", func(t *testing.T) {
+		testFn(true, 1, 0)
+	})
+
+	t.Run("shared-2", func(t *testing.T) {
+		testFn(true, 500, 0)
+	})
+}
+
+func TestFileLockGuard_Release(t *testing.T) {
+	path := "/tmp/FLOCK"
+	defer os.Remove(path)
+
+	lock, err := AcquireFileLock(path, false)
+	assert.Nil(t, err)
+	err = lock.Release()
+	assert.Nil(t, err)
+}
+
+func TestSyncDir(t *testing.T) {
+	path := "/tmp/test-sync/"
+	err := os.MkdirAll(path, os.ModePerm)
+	assert.Nil(t, err)
+	_, err = os.OpenFile(path+"test.txt", os.O_CREATE, 0644)
+	assert.Nil(t, err)
+	defer os.RemoveAll(path)
+
+	err = SyncDir(path)
+	assert.Nil(t, err)
 }
