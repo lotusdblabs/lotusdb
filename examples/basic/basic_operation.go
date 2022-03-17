92c92cfb7d9d3c4cedfc080f4769d93682022361
roseduan <roseduan520@gmail.com>
2022-03-18 00:31:26 +0800

    add examples baisc

--- examples/basic/basic_operation.go
+++ examples/basic/basic_operation.go
@@ -3,8 +3,15 @@ package main
 import (
 	"github.com/flower-corp/lotusdb"
 	"io/ioutil"
+	"time"
 )
 
+// basic operations for LotusDB:
+// put
+// put with options
+// get
+// delete
+// delete with options
 func main() {
 	path, _ := ioutil.TempDir("", "lotusdb")
 	opts := lotusdb.DefaultOptions(path)
@@ -12,10 +19,48 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
-	k := []byte("1")
-	err = db.Put(k, []byte("lotusdb"))
+	// 1.----put----
+	key1 := []byte("name")
+	err = db.Put(key1, []byte("lotusdb"))
 	if err != nil {
-		panic(err)
+		// ...
+	}
+
+	key2 := []byte("feature")
+	// 2.----put with options----
+	writeOpts := &lotusdb.WriteOptions{
+		Sync:      true,
+		ExpiredAt: time.Now().Add(time.Second * 100).Unix(),
+	}
+	err = db.PutWithOptions(key2, []byte("store data"), writeOpts)
+	if err != nil {
+		// ...
+	}
+
+	// 3.----get----
+	val, err := db.Get(key1)
+	if err != nil {
+		// ...
+	}
+	if len(val) > 0 {
+		// ...
+	}
+
+	// 4.----delete----
+	err = db.Delete(key1)
+	if err != nil {
+		// ...
+	}
+
+	// 5.----delete with options----
+	deleteOpts := &lotusdb.WriteOptions{
+		Sync:       false,
+		DisableWal: true,
+	}
+	err = db.DeleteWithOptions([]byte("dummy key"), deleteOpts)
+	if err != nil {
+		// ...
 	}
 }
