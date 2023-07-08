300880510e8fd0063058d2c6096c3e479cd819bd
wnz27 <wnz27@icloud.com>
2023-07-09 02:17:26 +0800

    feat: add test scale design

--- util/benchmark.go
+++ util/benchmark.go
@@ -2,7 +2,7 @@
  * @Author: 27
  * @LastEditors: 27
  * @Date: 2023-07-09 00:54:56
- * @LastEditTime: 2023-07-09 01:58:27
+ * @LastEditTime: 2023-07-09 02:16:51
  * @FilePath: /lotusdb-learn/util/benchmark.go
  * @description: type some description
  */
@@ -45,4 +45,19 @@ func keysNum(maxNum int) int {
 		return DefaultKeyNum
 	}
 	return maxNum
+}
+
+type TestScale struct {
+	KeysNum int // numbers of key
+
+	// calculate random size with min and max, key and value
+	MinKeySize   int
+	MaxKeySize   int
+	MinValueSize int
+	MaxValueSize int
+
+	Concurrency int    // concurrency run numbers, goroutines in go.
+	Path        string // database path
+	Compact     bool   // is benchmark compaction. write keys twice and run compaction after
+	Collection  bool   // is benchmark collection. write some keys and delete keys, then use collection.
 }
