90fbd05f56b74423fbe2cfc48ad1620c1c49b1a9
yanxiaoqi932 <pwq93208@163.com>
2023-07-11 08:35:33 +0000

    vlog write/read concurrently

--- vlog.go
+++ vlog.go
@@ -1,6 +1,10 @@
 package lotusdb
 
 import (
+	"errors"
+	"fmt"
+
+	"github.com/lotusdblabs/lotusdb/v2/util"
 	"github.com/rosedblabs/wal"
 )
 
@@ -8,10 +12,13 @@ const (
 	valueLogFileExt = ".VLOG"
 )
 
+var ErrWriteVlog = errors.New("Write vLog error")
+
 // valueLog value log is named after the concept in Wisckey paper
 // https://www.usenix.org/system/files/conference/fast16/fast16-papers-lu.pdf
 type valueLog struct {
-	wal *wal.WAL
+	wals        []*wal.WAL
+	numPartions uint32
 }
 
 type valueLogOptions struct {
@@ -25,43 +32,259 @@ type valueLogOptions struct {
 	// A block cache is used to store recently accessed data blocks, improving read performance.
 	// If BlockCache is set to 0, no block cache will be used.
 	BlockCache uint32
+
+	// Value logs are partioned to serveral parts for concurrent writing and reading
+	NumPartions uint32
+}
+
+// an auxiliary struct for returning chunkPositions in correct order
+// while writing or reading batch
+type seq struct {
+	buf    []byte
+	id     int
+	walpos *wal.ChunkPosition
+	part   int
+}
+
+// record accurate position in multiple vlogs
+type VlogPosition struct {
+	part   int
+	walPos *wal.ChunkPosition
 }
 
 func openValueLog(options valueLogOptions) (*valueLog, error) {
-	vLogWal, err := wal.Open(wal.Options{
-		DirPath:        options.DirPath,
-		SegmentSize:    options.SegmentSize,
-		SegmentFileExt: valueLogFileExt,
-		BlockCache:     options.BlockCache,
-		Sync:           false,
-		BytesPerSync:   0,
-	})
-	if err != nil {
-		return nil, err
+	vLogWals := []*wal.WAL{}
+
+	for i := 0; i < int(options.NumPartions); i++ {
+		vLogWal, err := wal.Open(wal.Options{
+			DirPath:        options.DirPath,
+			SegmentSize:    options.SegmentSize,
+			SegmentFileExt: fmt.Sprintf(".%d%s", i, valueLogFileExt),
+			BlockCache:     options.BlockCache,
+			Sync:           false,
+			BytesPerSync:   0,
+		})
+		if err != nil {
+			return nil, err
+		}
+		vLogWals = append(vLogWals, vLogWal)
 	}
-	return &valueLog{wal: vLogWal}, nil
+
+	return &valueLog{wals: vLogWals, numPartions: options.NumPartions}, nil
 }
 
-func (vlog *valueLog) read(position *wal.ChunkPosition) ([]byte, error) {
-	value, err := vlog.wal.Read(position)
-	if err != nil {
-		return nil, err
-	}
-	return value, nil
-}
-
-func (vlog *valueLog) write(data []byte) (*wal.ChunkPosition, error) {
-	pos, err := vlog.wal.Write(data)
+func (vlog *valueLog) write(data []byte, part int) (*wal.ChunkPosition, error) {
+	pos, err := vlog.wals[part].Write(data)
 	if err != nil {
 		return nil, err
 	}
 	return pos, nil
 }
 
+func (vlog *valueLog) read(position *VlogPosition) ([]byte, error) {
+	value, err := vlog.wals[position.part].Read(position.walPos)
+	if err != nil {
+		return nil, err
+	}
+
+	return value, nil
+}
+
+// we must maintain correct order with respect to input while returning chunkPositions
+func (vlog *valueLog) writeBatch(logs []*LogRecord) ([]*VlogPosition, error) {
+	// split logs into parts
+	logParts := [][]seq{}
+	for i := 0; i < int(vlog.numPartions); i++ {
+		logPart := []seq{}
+		logParts = append(logParts, logPart)
+	}
+	for i, log := range logs {
+		partIdx := vlog.getIndex(log.Key)
+		buf := encodeLogRecord(log)
+		logParts[partIdx] = append(logParts[partIdx], seq{buf: buf, id: i, part: partIdx})
+	}
+
+	// write parts concurrently
+	errChan := make(chan error, vlog.numPartions)
+	posChan := make(chan []seq, vlog.numPartions)
+	for i := 0; i < int(vlog.numPartions); i++ {
+		go func(part int) {
+			pos := []seq{}
+			for _, s := range logParts[part] {
+				p, err := vlog.write(s.buf, part)
+				if err != nil {
+					errChan <- err
+					posChan <- nil
+					return
+				}
+				pos = append(pos, seq{walpos: p, id: s.id, part: s.part})
+			}
+			errChan <- nil
+			posChan <- pos
+		}(i)
+	}
+
+	flag := false
+	positions := [][]seq{}
+	for i := 0; i < int(vlog.numPartions); i++ {
+		e := <-errChan
+		pos := <-posChan
+		if e != nil {
+			flag = true
+		}
+		positions = append(positions, pos)
+	}
+
+	if flag {
+		return nil, ErrWriteVlog
+	}
+
+	realPositions := vlog.mergePosSeqs(positions, len(logs))
+	return realPositions, nil
+}
+
+func (vlog *valueLog) readBatch(vlogPos []*VlogPosition) ([]*LogRecord, error) {
+	// split positions into parts
+	posParts := [][]seq{}
+	for i := 0; i < int(vlog.numPartions); i++ {
+		posPart := []seq{}
+		posParts = append(posParts, posPart)
+	}
+	for i, pos := range vlogPos {
+		partIdx := pos.part
+		walPos := pos.walPos
+		posParts[partIdx] = append(posParts[partIdx], seq{walpos: walPos, id: i, part: partIdx})
+	}
+
+	// read parts concurrently
+	errChan := make(chan error, vlog.numPartions)
+	bufChan := make(chan []seq, vlog.numPartions)
+	for i := 0; i < int(vlog.numPartions); i++ {
+		go func(part int) {
+			buf := []seq{}
+			for _, s := range posParts[part] {
+				b, err := vlog.read(&VlogPosition{part: s.part, walPos: s.walpos})
+				if err != nil {
+					errChan <- err
+					bufChan <- nil
+					return
+				}
+				buf = append(buf, seq{buf: b, id: s.id, part: s.part})
+			}
+			errChan <- nil
+			bufChan <- buf
+		}(i)
+	}
+
+	flag := false
+	bufs := [][]seq{}
+	for i := 0; i < int(vlog.numPartions); i++ {
+		e := <-errChan
+		buf := <-bufChan
+		if e != nil {
+			flag = true
+		}
+		bufs = append(bufs, buf)
+	}
+
+	if flag {
+		return nil, ErrWriteVlog
+	}
+
+	realBuf := vlog.mergeBufSeqs(bufs, len(vlogPos))
+	logRecords := []*LogRecord{}
+	for _, buf := range realBuf {
+		logRecords = append(logRecords, decodeLogRecord(buf))
+	}
+	return logRecords, nil
+}
+
 func (vlog *valueLog) sync() error {
-	return vlog.wal.Sync()
+	f := func(part int) error {
+		return vlog.wals[part].Sync()
+	}
+	return vlog.doConcurrent(f)
 }
 
 func (vlog *valueLog) close() error {
-	return vlog.wal.Close()
+	f := func(part int) error {
+		return vlog.wals[part].Close()
+	}
+	return vlog.doConcurrent(f)
+}
+
+func (vlog *valueLog) doConcurrent(f func(int) error) error {
+	errChan := make(chan error, vlog.numPartions)
+	for i := 0; i < int(vlog.numPartions); i++ {
+		go func(part int) {
+			err := f(part)
+			errChan <- err
+		}(i)
+	}
+
+	flag := false
+	for i := 0; i < int(vlog.numPartions); i++ {
+		e := <-errChan
+		if e != nil {
+			flag = true
+		}
+	}
+
+	if flag {
+		return ErrWriteVlog
+	}
+	return nil
+}
+
+func (vlog *valueLog) getIndex(key []byte) int {
+	return int(util.FnvNew32a(string(key)) % vlog.numPartions)
+}
+
+// multiplex merge sort for returning in correct order of input logs
+func (vlog *valueLog) mergePosSeqs(seqs [][]seq, numSeq int) []*VlogPosition {
+	pos := []*VlogPosition{}
+
+	for len(pos) < numSeq {
+		minId := numSeq + 1
+		minIdSeq := -1
+		for i := 0; i < int(vlog.numPartions); i++ {
+			if len(seqs[i]) > 0 && seqs[i][0].id < minId {
+				minId = seqs[i][0].id
+				minIdSeq = i
+			}
+		}
+		pos = append(pos, &VlogPosition{part: seqs[minIdSeq][0].part, walPos: seqs[minIdSeq][0].walpos})
+		if len(seqs[minIdSeq]) > 1 {
+			seqs[minIdSeq] = seqs[minIdSeq][1:]
+		} else {
+			seqs[minIdSeq] = []seq{}
+		}
+	}
+
+	return pos
+}
+
+// multiplex merge sort for returning in correct order of input VlogPositions
+func (vlog *valueLog) mergeBufSeqs(seqs [][]seq, numSeq int) [][]byte {
+	bufs := [][]byte{}
+
+	// multiplex merge sort
+	for len(bufs) < numSeq {
+		minId := numSeq + 1
+		minIdSeq := -1
+		for i := 0; i < int(vlog.numPartions); i++ {
+			if len(seqs[i]) > 0 && seqs[i][0].id < minId {
+				minId = seqs[i][0].id
+				minIdSeq = i
+			}
+		}
+		bufs = append(bufs, seqs[minIdSeq][0].buf)
+		if len(seqs[minIdSeq]) > 1 {
+			seqs[minIdSeq] = seqs[minIdSeq][1:]
+		} else {
+			seqs[minIdSeq] = []seq{}
+		}
+	}
+
+	return bufs
 }
---
+++ vlog_test.go
@@ -0,0 +1,139 @@
+package lotusdb
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/rosedblabs/wal"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVlogBasic(t *testing.T) {
+	opts := valueLogOptions{
+		DirPath:     "/tmp/lotusdb",
+		SegmentSize: wal.GB,
+		BlockCache:  32 * wal.KB * 10,
+		NumPartions: 5,
+	}
+
+	// test opening vlog
+	vlog, err := openValueLog(opts)
+	entries, err := ioutil.ReadDir(opts.DirPath)
+	for _, ent := range entries {
+		fmt.Printf("%v\n", ent)
+	}
+
+	require.Nil(t, err)
+
+	defer func() {
+		fmt.Printf("delete!\n")
+		for i := 0; i < int(vlog.numPartions); i++ {
+			vlog.wals[i].Delete()
+		}
+	}()
+
+	logs := [5]*LogRecord{
+		{Key: []byte("key 0"), Value: []byte("value 0")},
+		{Key: []byte("key 1"), Value: []byte("value 1")},
+		{Key: []byte("key 2"), Value: []byte("value 2")},
+		{Key: []byte("key 3"), Value: []byte("value 3")},
+		{Key: []byte("key 4"), Value: []byte("value 4")},
+	}
+	pos := []*VlogPosition{}
+
+	// test writing
+	t.Run("test writing", func(t *testing.T) {
+		pos, err = vlog.writeBatch(logs[:])
+		require.Nil(t, err)
+	})
+	vlog.sync()
+
+	// test reading
+	t.Run("test reading", func(t *testing.T) {
+		readLogs, err := vlog.readBatch(pos)
+		require.Nil(t, err)
+		for i := 0; i < len(logs); i++ {
+			fmt.Printf("%v\n", string(readLogs[i].Value))
+			require.Equal(t, logs[i], readLogs[i])
+		}
+	})
+
+}
+
+// numRW:500000
+// numPart:1 write:1.134225253s read:495.503395ms
+// numPart:3 write:623.370618ms read:429.974872ms
+// numPart:10 write:605.139844ms read:408.844017ms
+// numRW:1000000
+// numPart:1 write:2.253321174s read:840.806123ms
+// numPart:3 write:1.391289828s read:742.954468ms
+// numPart:10 write:1.27199545s read:752.774177ms
+// numRW:2000000
+// numPart:1 write:4.554865317s read:1.834398737s
+// numPart:3 write:3.521361612s read:1.667203604s
+// numPart:10 write:2.654927796s read:1.481536838s
+
+// Please set the go-test timeout long enough before run this test!
+func TestRWLarge(t *testing.T) {
+	opts := valueLogOptions{
+		DirPath:     "/tmp/lotusdb",
+		SegmentSize: wal.GB,
+		BlockCache:  32 * wal.KB * 10,
+		NumPartions: 1,
+	}
+	numRWList := []int{1000000, 2000000}
+	numPartList := []int{1, 3, 10}
+
+	for _, numRW := range numRWList {
+		fmt.Printf("numRW:%d\n", numRW)
+		for _, numPart := range numPartList {
+			fmt.Printf("numPart:%d ", numPart)
+			err := RWBatch(opts, numRW, numPart)
+			require.Nil(t, err)
+		}
+	}
+
+}
+
+func RWBatch(opts valueLogOptions, numRW int, numPart int) error {
+	opts.NumPartions = uint32(numPart)
+	vlog, err := openValueLog(opts)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		for i := 0; i < int(vlog.numPartions); i++ {
+			vlog.wals[i].Delete()
+		}
+	}()
+
+	val := strings.Repeat("v", 512)
+	logs := []*LogRecord{}
+	for i := 0; i < numRW; i++ {
+		log := &LogRecord{Key: []byte(fmt.Sprintf("%d", i)), Value: []byte(val)}
+		logs = append(logs, log)
+	}
+	start := time.Now()
+	pos, err := vlog.writeBatch(logs)
+	if err != nil {
+		return err
+	}
+	end := time.Since(start)
+	fmt.Printf("write:%v ", end)
+
+	vlog.sync()
+
+	start = time.Now()
+	_, err = vlog.readBatch(pos)
+	if err != nil {
+		return err
+	}
+	end = time.Since(start)
+	fmt.Printf("read:%v\n", end)
+
+	return nil
+}
