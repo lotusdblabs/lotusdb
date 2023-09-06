d8a3046b9cb46212949a8eb7a9c1ee5813241270
shaovie <43458943@qq.com>
2023-09-06 08:51:51 +0800

    defaultWriteOptions change to public

--- benchmark/bench_test.go
+++ benchmark/bench_test.go
@@ -33,7 +33,7 @@ func BenchmarkPut(b *testing.B) {
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		err := db.Put(util.GetTestKey(i), util.RandomValue(1024), defaultWriteOptions)
+		err := db.Put(util.GetTestKey(i), util.RandomValue(1024), lotusdb.DefaultWriteOptions)
 		assert.Nil(b, err)
 	}
 }
@@ -42,7 +42,7 @@ func BenchmarkGet(b *testing.B) {
 	destroy := openDB()
 	defer destroy()
 	for i := 0; i < 1000000; i++ {
-		err := db.Put(util.GetTestKey(i), util.RandomValue(128), defaultWriteOptions)
+		err := db.Put(util.GetTestKey(i), util.RandomValue(128), lotusdb.DefaultWriteOptions)
 		assert.Nil(b, err)
 	}
 	b.ReportAllocs()
