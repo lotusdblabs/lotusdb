1cb5953381ef51bae75c8b33a079e42a29461ea0
kani <yh_chan_kanio@163.com>
2024-07-11 17:14:03 +0800

    Add deprecatedtable and test, import google.uuid.

---
+++ deprecatedtable.go
@@ -0,0 +1,90 @@
+package lotusdb
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+const ()
+
+var (
+	ErrDtFull = errors.New("deprecatedtable full")
+)
+
+type (
+	// Deprecatedtable is used to store old information about deleted/updated keys. 
+	// for every write/update generated an uuid, we store uuid in the table.
+	// It is useful in compaction, allowing us to know whether the kv 
+	// in the value log is up-to-date without accessing the index.
+	//
+	// we always build deprecatedtable immediately after compaction,
+	deprecatedtable struct {
+		table   map[string][]uuid.UUID // we store deprecated uuid of keys,in memory
+		size	uint32
+		mu		sync.Mutex
+		options deprecatedtableOptions
+	}
+
+	deprecatedtableOptions struct {
+		capacity uint32
+	}
+)
+
+// Create a new deprecatedtable.
+func newDeprecatedTable(options deprecatedtableOptions) *deprecatedtable {
+    return &deprecatedtable{
+        table:   make(map[string][]uuid.UUID),
+		size: 0,
+        options: options,
+    }
+}
+
+func (dt *deprecatedtable) isFull() bool {
+	return dt.size >= dt.options.capacity
+}
+
+// Add a uuid to the specified key.
+func (dt *deprecatedtable) addEntry(key string, uuid uuid.UUID) error {
+	dt.mu.Lock()
+	defer dt.mu.Unlock()
+	if dt.isFull() {
+        return ErrDtFull
+    }
+	dt.size++
+    dt.table[key] = append(dt.table[key], uuid)
+	return nil
+}
+
+// Delete an entry by key and uuid.
+func (dt *deprecatedtable) removeEntry(key string, id uuid.UUID) {
+	dt.mu.Lock()
+	defer dt.mu.Unlock()
+    if _, exists := dt.table[key]; !exists {
+        return
+    }
+    // find and delete uuid
+    for i, v := range dt.table[key] {
+        if v == id {
+			dt.size--
+            dt.table[key] = append(dt.table[key][:i], dt.table[key][i+1:]...)
+            return
+        }
+    }
+}
+
+// Find if an uuid exists based on key and uuid.
+func (dt *deprecatedtable) existEntry(key string, id uuid.UUID) bool {
+	dt.mu.Lock()
+	defer dt.mu.Unlock()
+    if _, exists := dt.table[key]; !exists {
+        return false
+    }
+    for _, v := range dt.table[key] {
+        if v == id {
+            return true
+        }
+    }
+    return false
+}
---
+++ deprecatedtable_test.go
@@ -0,0 +1,95 @@
+package lotusdb
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestAddEntry(t *testing.T) {
+	options := deprecatedtableOptions{
+		capacity: 32,
+	}
+	dt := newDeprecatedTable(options)
+	uidNumber := 3
+	count := ((int)(options.capacity) - 4) / uidNumber
+	
+	for i := 0; i < count; i++ {
+		for j := 0; j < uidNumber; j++ {
+			uid := uuid.New()
+			dt.addEntry(string(rune(i)), uid)
+		}
+    }
+	if (int)(dt.size) != count * uidNumber {
+		t.Errorf("expected dt.size to be %d, got %d",count, dt.size)
+	}
+}
+
+func TestIsFull(t *testing.T) {
+	options := deprecatedtableOptions{
+		capacity: 32,
+	}
+	dt := newDeprecatedTable(options)
+	count := 31
+	for i := 0; i < count; i++ {
+        uid := uuid.New()
+		dt.addEntry(string(rune(i)), uid)
+    }
+	if dt.isFull() {
+		t.Errorf("expected not full dt.size:%d, capacity:%d",dt.size, dt.options.capacity)
+	}
+	uid := uuid.New()
+	dt.addEntry(string(rune(32)), uid)
+	if !dt.isFull() {
+		t.Errorf("expected full dt.size:%d, capacity:%d",dt.size, dt.options.capacity)
+	}
+}
+
+func TestUuidExist(t *testing.T) {
+	options := deprecatedtableOptions{
+		capacity: 32,
+	}
+	dt := newDeprecatedTable(options)
+	uidNumber := 3
+	count := ((int)(options.capacity) - 4) / uidNumber
+	
+	for i := 0; i < count; i++ {
+		for j := 0; j < uidNumber; j++ {
+			uid := uuid.New()
+			dt.addEntry(string(rune(i)), uid)
+			if !dt.existEntry(string(rune(i)),uid) {
+				t.Errorf("expected entry not exist! dt.size:%d, capacity:%d",dt.size, dt.options.capacity)
+			}
+		}
+    }
+	if (int)(dt.size) != count * uidNumber {
+		t.Errorf("expected dt.size to be %d, got %d",count, dt.size)
+	}
+}
+
+func TestRemoveEntry(t *testing.T) {
+	options := deprecatedtableOptions{
+		capacity: 32,
+	}
+	dt := newDeprecatedTable(options)
+	count := 31
+	var ids []uuid.UUID
+	for i := 0; i < count; i++ {
+        uid := uuid.New()
+		dt.addEntry(string(rune(i)), uid)
+		ids = append(ids, uid)
+		if !dt.existEntry(string(rune(i)),uid) {
+			t.Errorf("expected entry not exist! dt.size:%d, capacity:%d",dt.size, dt.options.capacity)
+		}
+    }
+
+	remove := 17 
+	for i := 0; i < remove ; i++ {
+		dt.removeEntry(string(rune(i)), ids[i])
+    }
+
+	if dt.size != (uint32(count-remove)) {
+		t.Errorf("expected size not match! dt.size:%d, capacity:%d",dt.size, dt.options.capacity)
+	}
+}
+
